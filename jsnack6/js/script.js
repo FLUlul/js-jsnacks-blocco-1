ce99a59ecce28991ba77d1ae8c297021f5606676
FLU <valerio.massj@gmail.com>
2021-10-28 15:20:16 +0200

    first look at jsnack 6

--- jsnack6/js/script.js
+++ jsnack6/js/script.js
@@ -1,1 +1,26 @@
 /* 6 - Genera un array di 50 numeri random. Nell’array non devono esserci doppioni. */
+
+/* 
+-creo un'array vuoto 
+-creo un ciclo per riempire l'array di 50 numeri random
+    -creo una variabile x che generi numeri random da 1 a 100
+    -se la variabile x e' diverso da indice array
+        -pusho variante x nell'array 
+-stampo l'array
+*/
+
+const arrNum = [];
+const arrNumDoppi = [];//test
+
+for(i=0; i<50; i++){
+    const x = Math.floor(Math.random()*100 +1);
+
+    if(x !== arrNum[i]){
+        arrNum.push(x);
+        console.log(x, arrNum[i]);
+    }else{//test
+        arrNumDoppi.push(x);//test
+    }
+}
+console.log(arrNum);
+console.log(arrNumDoppi);//test
