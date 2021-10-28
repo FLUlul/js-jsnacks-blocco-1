c8c2ff2354216832938578f7b4c3c1fdf8f26aa7
FLU <valerio.massj@gmail.com>
2021-10-28 13:51:13 +0200

    jsnack 3 done

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,2 +1,21 @@
 /* 3 - Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
+
+/* 
+-creo una variabile x = 0
+
+-creo un ciclo che si ripeta per 10 volte
+    -creo una variabile y che chiede all'utente di inserire un numero con un prompt
+    -addiziono il numero utente con la variabile x
+
+-stampo la variabile x
+*/
+
+let x = 0;
+
+for(i=0; i<10; i++){
+    let y = parseInt(prompt("Insert a number"));
+    x+=y;
+}
+
+console.log(x);
