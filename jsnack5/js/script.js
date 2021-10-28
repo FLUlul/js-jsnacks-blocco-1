6e1eb5df7ba14844b46e0c08fd78cdee53ec9267
FLU <valerio.massj@gmail.com>
2021-10-28 14:52:50 +0200

    jsnack 5 done

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,3 +1,23 @@
 /* 5 - Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
+
+/* 
+-creo un'array vuoto
+-creo un ciclo che duri 6 volte
+    -creo una costante x, chiedo all'utente di inserire un numero
+    -se la costante x e' un numero dispari
+        -pusho x nell' array
+-stampo gli indici dell'array
+*/
+
+const arrNum = [];
+
+for(i=0; i<6; i++){
+    const x = parseInt(prompt("Insert a number"));
+
+    if(x % 2 !== 0){
+        arrNum.push(x);
+    }    
+}
+console.log(`All the odd numbers are: ${arrNum}`);
